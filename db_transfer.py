e0101db60c18326afa3629f66af497ed0012da14
AntonUkhov <89222229263@mail.ru>
2022-10-20 12:58:14 +0500

    added transfers from several tables

--- db_transfer.py
+++ db_transfer.py
@@ -11,7 +11,7 @@ try:
         user=MYSQL_USER,
         password=MYSQL_PASS,
         database=MYSQL_NAME,
-        cursorclass=pymysql.cursors.DictCursor
+        cursorclass=pymysql.cursors.SSCursor
     )
     print('Connection to MySQL success')
     print("#" * 20)
@@ -30,14 +30,46 @@ try:
     try:
         #select all data from table mysql
         with mysql_conn.cursor() as my_cursor:
+            # bad_fraud
             my_cursor.execute("SELECT * FROM `bad_fraud`")
-            rows = my_cursor.fetchall()
+            row = my_cursor.fetchone()
+            while row is not None:
+                # insert data into table postgres
+                with pg_conn.cursor() as pg_cursor:
+                    value = row[1]
+                    pg_cursor.execute(f"INSERT INTO mcams_badfraud (bad_fraud) VALUES ('{value}')")
+                row = my_cursor.fetchone()
 
-        #insert data into table postgres
-        with pg_conn.cursor() as pg_cursor:
-            for row in rows:
-                value = str(row['value'])
-                pg_cursor.execute(f"INSERT INTO mcams_badfraud (bad_fraud) VALUES ('{value}')")
+            # bad_fraud_words
+            my_cursor.execute("SELECT * FROM `bad_fraud_words`")
+            row = my_cursor.fetchone()
+            while row is not None:
+                # insert data into table postgres
+                with pg_conn.cursor() as pg_cursor:
+                    value = row[1]
+                    pg_cursor.execute(f"INSERT INTO mcams_badfraudwords (bad_fraud_words) VALUES ('{value}')")
+                row = my_cursor.fetchone()
+
+            # bad_spam
+            my_cursor.execute("SELECT * FROM `bad_spam`")
+            row = my_cursor.fetchone()
+            while row is not None:
+                # insert data into table postgres
+                with pg_conn.cursor() as pg_cursor:
+                    value = row[1]
+                    pg_cursor.execute(f"INSERT INTO mcams_badspam (bad_spam) VALUES ('{value}')")
+                row = my_cursor.fetchone()
+
+            # bad_words
+            my_cursor.execute("SELECT * FROM `bad_words`")
+            row = my_cursor.fetchone()
+            while row is not None:
+                # insert data into table postgres
+                with pg_conn.cursor() as pg_cursor:
+                    value = row[1]
+                    pg_cursor.execute(f"INSERT INTO mcams_badwords (bad_words) VALUES ('{value}')")
+                row = my_cursor.fetchone()
+
     finally:
         if mysql_conn:
             mysql_conn.close()
